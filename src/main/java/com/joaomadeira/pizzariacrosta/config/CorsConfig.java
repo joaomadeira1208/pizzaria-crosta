36a81f51f55f2af5ee7686bd0d4152c5b34d0c1d
João Madeira <joaomadeira1208@gmail.com>
2025-05-09 19:07:00 -0300

    Simplifies CORS configuration.
    Removes unnecessary dependency injection and simplifies the property injection of the allowed origin in the CORS configuration.
    
    This makes the configuration cleaner and easier to understand.

--- src/main/java/com/joaomadeira/pizzariacrosta/config/CorsConfig.java
+++ src/main/java/com/joaomadeira/pizzariacrosta/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.joaomadeira.pizzariacrosta.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +7,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class CorsConfig {
 
     @Value("${cors.origem}")
-    private final String ORIGEM;
+    private String origem;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -20,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permite todos os endpoints
-                        .allowedOrigins(ORIGEM) // Permite chamadas dessa origem
+                        .allowedOrigins(origem) // Permite chamadas dessa origem
                         .allowedMethods("*") // GET, POST, PUT, DELETE, etc.
                         .allowedHeaders("*");
             }
